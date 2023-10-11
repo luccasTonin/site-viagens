1823ae50ed05369c538b7afe2f4d674684d9fa7e
Lucas <lucas.tonin@lbv.org.br>
2023-10-11 13:33:15 -0300

    Uni todas as mudanças em apenas um commit.

---
+++ js/main.js
@@ -0,0 +1,69 @@
+function validarEmail() {
+    var email = document.getElementById('email').value;
+
+    var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (regex.test(email)) {
+      alert('Email')
+    }
+}
+
+function validarEmail() {
+    var email = document.getElementById('email').value;
+
+    var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (regex.test(email)) {
+      alert('Email válido!');
+    } else {
+      alert('Email inválido! Por favor, insira um email válido')
+    }
+}
+
+function validarEmail() {
+    var email = document.getElementById('email').value;
+
+    var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (regex.test(email)) {
+      alert('Email válido!');
+    } else {
+      alert('Email inválido! Por favor, insira um email válido')
+    }
+}
+
+function validarEmail() {
+    var email = document.getElementById('email').value;
+
+    var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (regex.test(email)) {
+      alert('Email válido!');
+    } else {
+      alert('Email inválido! Por Favor, insira um email válido')
+    }
+}
+
+function validarEmail() {
+    var email = document.getElementById('email').value;
+
+    var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (regex.test(email)) {
+      alert('Email válido!');
+    } else {
+      alert('Email inválid0! Por favor, insira um email válido')
+    }
+}
+
+function validarEmail(){
+    var email = document.getElementById('email').value;
+
+    var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (regex.test(email)) {
+      alert('Email válido!');
+    } else {
+      alert('Email inválido! Por favor, insira um email válido.');
+    }
+  }
